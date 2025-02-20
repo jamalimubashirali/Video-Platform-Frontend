60b11b1e986654ec42d9fb7e54cd997d255cacff
Mubashir Ali <21sw071@students.muet.edu.pk>
2025-02-20 10:03:19 +0500

    Added authentication Service

---
+++ src/services/auth.service.js
@@ -0,0 +1,55 @@
+import {config} from '../config';
+import axios from 'axios';
+
+class Authentication {
+    constructor(){
+        this.baseURL = config.backendUrl;
+    }
+
+    async register(email, password, fullname , username , avatar , converImage){
+        try {
+            const response = await axios.post(`${this.baseURL}/users/register`, {
+                email,
+                password,
+                fullname,
+                username,
+                avatar,
+                converImage
+            } , {
+                withCredentials: true
+            });
+            return response.data;
+        } catch (error) {
+            console.log(`Error Creating User: ${error}`);
+        }
+    }
+
+    async login(email, password){
+        try {
+            const response = await axios.post(`${this.baseURL}/users/login`, {
+                email,
+                password
+            } , {
+                withCredentials: true
+            });
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async logout(){
+        try {
+            const response = await axios.post(`${this.baseURL}/users/logout` , {
+                withCredentials: true
+            });
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+const authService = new Authentication();
+
+export default authService;
